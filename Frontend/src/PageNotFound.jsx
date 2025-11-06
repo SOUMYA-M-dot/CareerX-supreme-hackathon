a0d5353f33eec3196c2d2473f7a9edebe02fdf45
hritam04.samanta@gmail.com <hritam04.samanta@gmail.com>
2025-11-06 09:14:55 +0530

    page not found little fix

--- Frontend/src/PageNotFound.jsx
+++ Frontend/src/PageNotFound.jsx
@@ -1,35 +1,66 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import CustomCursor from './components/CustomCursor'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import CustomCursor from "./components/CustomCursor";
 
 const PageNotFound = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className='relative w-[95vw] h-[90vh] flex flex-col items-center justify-center cursor-default'>
+    <div className="relative w-[95vw] h-[90vh] flex flex-col items-center justify-center cursor-default">
       <CustomCursor />
-      <div className='flex flex-col w-full items-center lg:justify-end h-[30%] lg:h-[40%]'>
-        <h1 className='hero-text text-[13vw] lg:text-[9vw] leading-none text-transparent mb-3 lg:mb-2' style={{
-    WebkitTextStroke: "1px white",
-  }}>Page not found</h1>
-        <p className='text-center text-[3vw] lg:text-[1.2vw] font-thin leading-none mb-8 lg:mb-4'>Unfortunately, the page you are looking for does not exist. <br />But there are other useful pages:</p>
-        <div className='flex hero-text text-[5vw] lg:text-[1.2vw] gap-[5vw] lg:gap-[2vw] underline '>
-          <h4 onClick={() => {
-            navigate("/home");
-          }}>Home</h4>
-          <h4 onClick={() => {
-            navigate("/career");
-          }}>Career</h4>
-          <h4 onClick={() => {
-            navigate("/about");
-          }}>About</h4>
+      <div className="flex flex-col w-full items-center lg:justify-end h-[30%] lg:h-[40%]">
+        <h1
+          className="hero-text text-[13vw] lg:text-[9vw] leading-none text-transparent mb-3 lg:mb-2 flex gap-[4vw] lg:gap-[2vw]"
+          style={{
+            WebkitTextStroke: "1px white",
+          }}
+        >
+          {" "}
+          <span className="hover:text-white transition-all delay-300">Page</span>
+          <span className="hover:text-white transition-all delay-300">not</span>
+          <span className="hover:text-white transition-all delay-300">found</span>{" "}
+        </h1>
+        <p className="text-center text-[3vw] lg:text-[1.2vw] font-thin leading-none mb-8 lg:mb-4">
+          Unfortunately, the page you are looking for does not exist. <br />
+          But there are other useful pages:
+        </p>
+        <div className="flex hero-text text-[5vw] lg:text-[1.2vw] gap-[5vw] lg:gap-[2vw] underline ">
+          <h4
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            Home
+          </h4>
+          <h4
+            onClick={() => {
+              navigate("/career");
+            }}
+          >
+            Career
+          </h4>
+          <h4
+            onClick={() => {
+              navigate("/about");
+            }}
+          >
+            About
+          </h4>
         </div>
       </div>
-      <div className='relative w-[95vw] h-[20vw]'>
-        <img className='h-[25vw] lg:h-[8vw] lg:opacity-0 absolute z-999 -top-[50%] lg:top-[20%] left-[10%] lg:left-[30%] hover:opacity-100 transition-all' src="/images/r1.png" alt="" />
-        <img className='h-[25vw] lg:h-[8vw] lg:opacity-0 absolute z-999 top-1/2 right-[10%] lg:right-[32%] hover:opacity-100 transition-all' src="/images/r2.png" alt="" />
+      <div className="relative w-[95vw] h-[20vw]">
+        <img
+          className="h-[29vw] lg:h-[8vw] lg:opacity-0 absolute z-999 -top-[50%] lg:top-[20%] left-[10%] lg:left-[30%] hover:opacity-100 transition-all delay-100"
+          src="/images/r1.png"
+          alt=""
+        />
+        <img
+          className="h-[29vw] lg:h-[8vw] lg:opacity-0 absolute z-999 top-1/2 right-[10%] lg:right-[32%] hover:opacity-100 transition-all delay-100"
+          src="/images/r2.png"
+          alt=""
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
